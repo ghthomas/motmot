aa699439378be6388c7901cefd20867509bfe1d0
ghthomas <gavin.thomas@sheffield.ac.uk>
2013-06-21 11:22:00 +0100

    Added deprecated geiger function name.check.R

---
+++ R/name.check.R
@@ -0,0 +1,17 @@
+name.check <- function (phy, data) 
+{
+    if (is.vector(data)) {
+        data.names <- names(data)
+    }
+    else {
+        data.names <- rownames(data)
+    }
+    t <- phy$tip.label
+    r1 <- t[is.na(match(t, data.names))]
+    r2 <- data.names[is.na(match(data.names, t))]
+    r <- list(sort(r1), sort(r2))
+    names(r) <- cbind("tree_not_data", "data_not_tree")
+    if (length(r1) == 0 && length(r2) == 0) 
+	return("OK")
+    else return(r)
+}
